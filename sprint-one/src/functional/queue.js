a744a0eb27842a2d6c0dfc14d355e3de32215757
Minlu Jiang <ninglv111@gmail.com>
2020-02-05 14:58:16 -0800

    completed the basic queue and stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,4 +1,6 @@
 var Queue = function() {
+  var enqPos = 0;
+  var deqPos = 0;
   var someInstance = {};
 
   // Use an object with numeric keys to store values
@@ -7,12 +9,19 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[enqPos++] = value;
   };
 
   someInstance.dequeue = function() {
+    if (deqPos >= enqPos) return null;
+    var deqVal = storage[deqPos++];
+    delete storage[deqPos - 1];
+    return deqVal;
   };
 
   someInstance.size = function() {
+    console.log("length of the queue " + (enqPos - deqPos));
+    return enqPos - deqPos;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,28 @@
 var Stack = function() {
-  var someInstance = {};
+  var count = 0;
+  var someInstance = {
+
+  };
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[++count] = value;  
   };
 
   someInstance.pop = function() {
+    if (count === 0) {
+      return null;
+    }
+    var popVal = storage[count--];
+    delete storage[count + 1];
+    return popVal;
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
