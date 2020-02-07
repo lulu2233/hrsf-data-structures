3fe6d674ed5d55610b051f3f42360726faa25648
Minlu Jiang <ninglv111@gmail.com>
2020-02-06 19:56:59 -0800

    start the sprint2, have completed the linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,37 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.tail === null) {
+      list.tail = newNode; 
+    }
+    if (list.head === null) {
+      list.head = newNode;
+    }
+    var lastNode = list.tail;
+    lastNode.next = newNode;
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
+    if (list.head === null) return null;
+    var headNode = list.head;
+    list.head = headNode.next;
+    return headNode.value;
   };
 
   list.contains = function(target) {
+    var start = list.head;
+    while(start !== null) {
+      if (start.value === target) {
+        return true;
+      }
+      start = start.next;
+    }
+    return false;
   };
 
   return list;
